8cc2887529bc5affbc5ff48ad34b576e1a311163
talum <tracidini@gmail.com>
2016-02-01 22:19:21 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#The Wisdoms of Jeffrey
+
+A single-page app celebrating the many wisdoms of the brilliant Jeff Katz. 
+
+Submit more wisdoms [here](mailto: tracy.lum@flatironschool.com).
---
+++ js/app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(event){
+  loadData();
+  buttonListener();
+});
+
+function loadData(){
+  var num = Math.floor(Math.random() * data.length);
+  document.getElementById('quote').textContent = data[num].quote;
+  document.getElementById('follow-up').textContent = data[num].followUp;
+  document.getElementById('context').textContent = data[num].context;
+}
+
+function buttonListener(){
+  document.getElementById('knowledge-button').onclick = function(){
+    loadData();
+
+  }
+}
